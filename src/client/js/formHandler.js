ed030957ea71fa1dbfad4974f18ac641bb0be412
Micha Kraußer <michak@synphonyte.com>
2021-02-05 08:53:21 -0600

    set up WheaterAPI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,35 @@
-function handleSubmit(event) {
+/* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
+// Personal API Key for OenWeatherMap API
+const apiKey = '&appid=18f4bd3e4bb4ccb550b483c6cf16eb31&units=imperial';
+
+function handleSubmit( event ) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let zip = document.getElementById('name').value
+    // Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:3333/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    fetch( baseURL + zip + apiKey )
+        .then( res => res.json() )
+        .then(function( res ) {
+
+            // date
+            let d = new Date();
+            const months = [ "January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December" ];
+            const today = d.getDate() + '. ' + months[ d.getMonth() ] + ' ' + d.getFullYear();
+
+            console.log( 'WheaterData: ', res );
+            console.log( 'date: ', document.getElementById( 'date' ).innerHTML );
+
+            document.getElementById( 'location' ).innerHTML = `Weather data in ${ res.name }`;
+            document.getElementById( 'date' ).innerHTML = `Date: ${ today }`;
+            document.getElementById( 'temp' ).innerHTML = `Temperature: ${ res.main.temp } °`;
+        }
+    )
 }
 
 export { handleSubmit }
